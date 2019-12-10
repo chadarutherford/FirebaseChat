fc39fa00d34403b5f5826e5c0bdaba75e1d9cb34
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-10 11:38:35 -0500

    Initial Commit

---
+++ FirebaseChat/Controller/LoginController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginController.swift
+//  FirebaseChat
+//
+//  Created by Chad Rutherford on 12/10/19.
+//  Copyright © 2019 chadarutherford.com. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+}
